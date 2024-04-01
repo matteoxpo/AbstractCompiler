21eff92fbbfa53d99114b099eeabea3f8d8e51db
Sereja Khromin <s-hromin@mail.ru>
2024-04-01 08:45:55 +0700

    fix text size changing

--- Compiler/Models/HtmlService.cs
+++ Compiler/Models/HtmlService.cs
@@ -8,9 +8,6 @@ public static class HtmlService
     public static void Open(string path)
     {
         new Process() { StartInfo = new ProcessStartInfo(path) { UseShellExecute = true }}.Start();
-        //if (!System.IO.File.Exists(path) && !IsUrl(path))
-            //throw new ArgumentException($"Path: \'{path}\' is incorrect");
-
     }
 
     public static bool IsUrl(string input) => Regex.IsMatch(input, @"^(https?|ftp)://[^\s/$.?#].[^\s]*$");
